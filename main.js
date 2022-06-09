23edb9049b231d5a648d6880bc3ff618c54a4faa
Md Reaj Uddin Rabbi <mdreajuddinrabbi@gmail.com>
2022-06-09 21:00:29 +1000

    fetching and searching words from api
    only displaying results for non existing words

---
+++ main.js
@@ -0,0 +1,36 @@
+const wrapper = document.querySelector(".wrapper");
+const searchInput = wrapper.querySelector("input");
+const infoText = wrapper.querySelector(".info-text");
+
+function data(result, word) {
+  if (result.title) {
+    infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+  }
+}
+
+//word search function
+function search(word) {
+  fetchApi(word);
+  searchInput.value = word;
+}
+
+//fetch api funtion
+function fetchApi(word) {
+  wrapper.classList.remove("active");
+  infoText.style.color = "#000";
+  infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => data(result, word))
+    .catch(() => {
+      infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+    });
+}
+
+searchInput.addEventListener("keyup", (e) => {
+  let word = e.target.value.replace(/\s+/g, " ");
+  if (e.key == "Enter" && word) {
+    fetchApi(word);
+  }
+});
