780ca3c686abd689623af169d40b299dc57997df
Md Reaj Uddin Rabbi <mdreajuddinrabbi@gmail.com>
2022-06-09 21:11:13 +1000

    displaying meanings
    bug: example going into meaning class

--- main.js
+++ main.js
@@ -5,6 +5,29 @@ const infoText = wrapper.querySelector(".info-text");
 function data(result, word) {
   if (result.title) {
     infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+  } else {
+    wrapper.classList.add("active");
+    let definitions = result[0].meanings[0].definitions[0],
+      phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
+    document.querySelector(".word p").innerText = result[0].word;
+    document.querySelector(".word span").innerText = phontetics;
+    document.querySelector(".meaning span").innerText = definitions.definition;
+    document.querySelector(".example span").innerText = definitions.example;
+    audio = new Audio("https:" + result[0].phonetics[0].audio);
+    if (definitions.synonyms[0] == undefined) {
+      synonyms.parentElement.style.display = "none";
+    } else {
+      synonyms.parentElement.style.display = "block";
+      synonyms.innerHTML = "";
+      for (let i = 0; i < 5; i++) {
+        let tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[i]},</span>`;
+        tag =
+          i == 4
+            ? (tag = `<span onclick="search('${definitions.synonyms[i]}')">${definitions.synonyms[4]}</span>`)
+            : tag;
+        synonyms.insertAdjacentHTML("beforeend", tag);
+      }
+    }
   }
 }
 
